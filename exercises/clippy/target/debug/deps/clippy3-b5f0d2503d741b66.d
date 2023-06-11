2ff7bdba7efe3b61407fc166fe26cacd848f615b
jack <xinliang.ji@qq.com>
2023-06-11 16:16:32 +0800

    add the solutions files

---
+++ exercises/clippy/target/debug/deps/clippy3-b5f0d2503d741b66.d
@@ -0,0 +1,8 @@
+/home/jack/Downloads/rustlings/exercises/clippy/target/debug/deps/clippy3-b5f0d2503d741b66.rmeta: clippy3.rs Cargo.toml
+
+/home/jack/Downloads/rustlings/exercises/clippy/target/debug/deps/clippy3-b5f0d2503d741b66.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
